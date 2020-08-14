56bc604009e18eccb376cac78d92af7ffd23ffec
kvsiva <sivakrishna.konijeti@gmail.com>
2020-08-14 19:22:24 +0530

    constructer added for Counter component using shortcut rconst followed by tab

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 
 export class Counter extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             counter: 0
+        }
+    }
+    
     render() {
         console.log('Render')
         return (
             <div className="counter">
-                Counter: {0}
+                Counter: {this.state.counter}
             </div>
         )
     }
