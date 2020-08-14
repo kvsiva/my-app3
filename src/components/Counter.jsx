cf1833df7a64aea6553e9f2a12af73b97f2057b2
kvsiva <sivakrishna.konijeti@gmail.com>
2020-08-14 20:09:57 +0530

    added buttons and actions

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,18 +2,26 @@ import React, { Component } from 'react'
 
 export class Counter extends Component {
     constructor(props) {
+        console.log('Constructor')
         super(props)
     
         this.state = {
              counter: 0
         }
+        this.increment = () => this.setState({counter:this.state.counter+1})
+        this.decrement = () => this.setState({counter:this.state.counter-1})
+        
     }
     
     render() {
         console.log('Render')
         return (
+            <div> 
+            <button onClick={this.increment}>Increment</button>      
+            <button onClick={this.decrement}>Decrement</button>
             <div className="counter">
                 Counter: {this.state.counter}
+            </div>
             </div>
         )
     }
